aff8c5f2ea9c480d77b6ed83db3f240da93616cf
Razymo37 <raphalenaul14@gmail.com>
2024-04-08 10:57:35 +0200

    deplacement menu shop fonctionnel à faire adaptation de l'éacran

--- app/src/main/java/com/example/snakedroid/Shop.java
+++ app/src/main/java/com/example/snakedroid/Shop.java
@@ -1,24 +1,37 @@
 package com.example.snakedroid;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import com.example.snakedroid.databinding.ActivityShopBinding;
 
 public class Shop extends AppCompatActivity {
 
+    private ActivityShopBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_shop);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        binding = ActivityShopBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+
+        Intent intent_menu = new Intent(Shop.this,MainActivity.class);
+
+        binding.buttonExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(intent_menu);
+            }
+        });
+
+
+
+    }
+
 }
