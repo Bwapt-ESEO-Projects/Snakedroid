e3532588d578996ba9cbf80653b4710ce3a2cf55
henaulra <raphael.henault@reseau.eseo.fr>
2024-04-08 09:40:49 +0200

    ajout fragment et bouton main page fonctionnel

--- app/src/main/java/com/example/snakedroid/MainActivity.java
+++ app/src/main/java/com/example/snakedroid/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.snakedroid;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,44 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.snakedroid.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+
+    }
+
+    @Override
+    public void onResume()
+    {
+        Intent intent_game= new Intent(MainActivity.this,Game.class);
+        Intent intent_shop=new Intent(MainActivity.this,Shop.class);
+        super.onResume();
+        binding.buttonPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+               startActivity(intent_game);
+
+
+            }
         });
+
+        binding.buttonShop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(intent_shop);
+            }
+        });
+
     }
 }
